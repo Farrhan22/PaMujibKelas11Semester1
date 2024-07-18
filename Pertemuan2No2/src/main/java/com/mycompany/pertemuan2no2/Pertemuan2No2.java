a7c4c02ed1d349aabca83366ca48206b4a19897c
fa2han <142730380+Farrhan22@users.noreply.github.com>
2024-07-18 13:52:57 +0700

    Add files via upload

---
+++ Pertemuan2No2/src/main/java/com/mycompany/pertemuan2no2/Pertemuan2No2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2no2;
+
+/**
+ *
+ * @author Farhannn
+ */
+public class Pertemuan2No2 {
+public static String Makanan;
+public static final String Minuman = "Jus Jambu";
+public static void main(String[] args) {
+ // TODO code application logic here
+ Makanan = "Ayam Geprek";//Mengisi Value pada variable Hobi
+ System.out.println("Makanan Favorit Saya " + Makanan);
+ System.out.println("Minuman Favorit Saya " + Minuman);
+ }
+}
